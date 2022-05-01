3a279440be4629d89b9c1914838da26256af3a21
Jcduhdt <15150651313@126.com>
2022-05-01 16:13:43 +0800

    add tracker

---
+++ tools/go_torrent/torrent/tracker_test.go
@@ -0,0 +1,21 @@
+package torrent
+
+import (
+    "bufio"
+    "fmt"
+    "github.com/stretchr/testify/assert"
+    "os"
+    "testing"
+)
+
+func TestTracker(t *testing.T) {
+    file, err := os.Open("../testfile/debian-iso.torrent")
+    assert.Equal(t, err, nil)
+    tf, err := ParseFile(bufio.NewReader(file))
+    assert.Equal(t, err, nil)
+
+    peers := FindPeers(tf)
+    for idx, peer := range peers {
+        fmt.Printf("Peer %d,Ip: %s,Port:%d \n", idx, peer.Ip, peer.Port)
+    }
+}
